40dc821200c1d9a6850a9ef966009980bcac804b
xuyong <xuyongshuaige@gmail.com>
2022-10-29 14:36:20 +0800

    feat: 实现随机三角平面组合

--- demo04/index.js
+++ demo04/index.js
@@ -13,23 +13,34 @@ scene.add(camera)
 // 物体：几何体 + 材质
 // const geomentry = new THREE.BoxGeometry(1,1,1)
 // 通过buffer创建几何体，设置position
-const geomentry = new THREE.BufferGeometry()
-const vertiest = new Float32Array([
-    0, 0, 1,
-    1, 0, 1,
-    0, 1, 1,
-    1, 1, 1,
-    0, 1, 1,
-    1, 0, 1
-])
-geomentry.setAttribute('position', new THREE.BufferAttribute(vertiest, 3))
+// const geomentry = new THREE.BufferGeometry()
+// const vertiest = new Float32Array([
+//     0, 0, 1,
+//     1, 0, 1,
+//     0, 1, 1,
+//     1, 1, 1,
+//     0, 1, 1,
+//     1, 0, 1
+// ])
+// geomentry.setAttribute('position', new THREE.BufferAttribute(vertiest, 3))
+// const material = new THREE.MeshBasicMaterial({ color:  0x00ff00 }) 
+// const cube = new THREE.Mesh(geomentry, material)
 
-const material = new THREE.MeshBasicMaterial({ color:  0x00ff00 }) 
-const cube = new THREE.Mesh(geomentry, material)
+// 随机生成多个cube
+for(let i = 0; i < 50 ; i++) {
+    const geomentry = new THREE.BufferGeometry()
+    const material = new THREE.MeshBasicMaterial({ color: new THREE.Color(Math.random(), Math.random(), Math.random()), transparent: true, opacity: Math.random()})
+    const vertiest = new Float32Array(9)
+    for(let j = 0; j < 9; j++) {
+        vertiest[j] = Math.random() * 5
+    }
+    geomentry.setAttribute('position', new THREE.BufferAttribute(vertiest, 3))
+    const cube = new THREE.Mesh(geomentry, material)
+    // console.log(cube)
+    scene.add(cube)
+}
 
-console.log('geomentry', geomentry)
-
-scene.add(cube)
+// scene.add(cube)
 // 渲染器：canvas 加入到domcument里面去
 const renderer = new THREE.WebGL1Renderer()
 renderer.setSize(window.innerWidth, window.innerWidth)
