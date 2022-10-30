28fe322026759c0219dba2ffcce09e79bae2259f
xuyong <xuyongshuaige@gmail.com>
2022-10-30 23:18:04 +0800

    feat-使用贴图

--- demo06/index.js
+++ demo06/index.js
@@ -1,46 +1,57 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
-
-
-
 // 场景: 角度 远景 进景 大小
 const scene = new THREE.Scene() 
 // 镜头
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000) //TODO 0.1 --> 0的话就没有显示
 camera.position.z = 5
 scene.add(camera)
-// 物体：几何体 + 材质
-// const geomentry = new THREE.BoxGeometry(1,1,1)
-// 通过buffer创建几何体，设置position
-// const geomentry = new THREE.BufferGeometry()
-// const vertiest = new Float32Array([
-//     0, 0, 1,
-//     1, 0, 1,
-//     0, 1, 1,
-//     1, 1, 1,
-//     0, 1, 1,
-//     1, 0, 1
-// ])
-// geomentry.setAttribute('position', new THREE.BufferAttribute(vertiest, 3))
-// const material = new THREE.MeshBasicMaterial({ color:  0x00ff00 }) 
-// const cube = new THREE.Mesh(geomentry, material)
 
-// 随机生成多个cube
-for(let i = 0; i < 50 ; i++) {
-    const geomentry = new THREE.BufferGeometry()
-    const material = new THREE.MeshBasicMaterial({ color: new THREE.Color(Math.random(), Math.random(), Math.random()), transparent: true, opacity: Math.random()})
-    const vertiest = new Float32Array(9)
-    for(let j = 0; j < 9; j++) {
-        vertiest[j] = Math.random() * 5
-    }
-    geomentry.setAttribute('position', new THREE.BufferAttribute(vertiest, 3))
-    const cube = new THREE.Mesh(geomentry, material)
-    // console.log(cube)
-    scene.add(cube)
-}
+const loader = new THREE.TextureLoader()
+const doorTexture = loader.load('./textures/door/color.jpg')
+const doorAoTexture  = loader.load('./textures/door/ambientOcclusion.jpg')
+const doorAlphaTexture = loader.load('./textures/door/alpha.jpg')
 
-// scene.add(cube)
+// 置换贴图
+const doorHeightTexture = loader.load('./textures/door/height.jpg')
+const doorMetalnessTexture = loader.load('./textures/door/metalness.jpg')
+const doorRoughnessTexture = loader.load('./textures/door/roughness.jpg')
+const doorNormalTexture = loader.load('./textures/door/normal/jpg')
+
+const geomentry = new THREE.BoxGeometry(1, 1, 1, 100, 100)
+
+const meterial = new THREE.MeshStandardMaterial({
+    map: doorTexture,
+    aoMap: doorAoTexture,
+    alphaMap: doorAlphaTexture,
+    displacementMap: doorHeightTexture,
+    displacementScale: 0.1,
+    metalnessMap: doorMetalnessTexture,
+    metalness: 1,
+    roughnessMap: doorRoughnessTexture,
+    roughness: 1,
+    normalMap: doorNormalTexture,
+    transparent: true
+})
+
+geomentry.setAttribute('uv2', new THREE.BufferAttribute(geomentry.attributes.uv.array, 2))
+
+const cube = new THREE.Mesh(geomentry, meterial)
+scene.add(cube)
+
+// 添加环境灯光
+const light = new THREE.AmbientLight( 0x404040 ) // soft white light
+scene.add(light)
+
+// 添加直射光
+const directionalLight = new THREE.DirectionalLight( 0xffffff)
+directionalLight.position.set(10, 10, 10)
+scene.add( directionalLight )
+
+
+console.log(directionalLight)
+
 // 渲染器：canvas 加入到domcument里面去
 const renderer = new THREE.WebGL1Renderer()
 renderer.setSize(window.innerWidth, window.innerWidth)
@@ -49,9 +60,6 @@ renderer.setSize(window.innerWidth, window.innerWidth)
 // 轨道控制器
 const controls = new OrbitControls( camera, renderer.domElement )
 document.body.appendChild(renderer.domElement)
-
-
-
 
 
 
